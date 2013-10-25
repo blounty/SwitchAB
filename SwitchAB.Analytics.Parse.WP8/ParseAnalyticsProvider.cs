430ebfbcd24cf365f642d0b318f17565c570c8f1
Alex Blount <alex.blount@re-systems.co.uk>
2013-10-25 15:40:12 +0100

    added WP8 sample

---
+++ SwitchAB.Analytics.Parse.WP8/ParseAnalyticsProvider.cs
@@ -0,0 +1,36 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchAB.Analytics.Parse
+{
+    public class ParseAnalyticsProvider : IAnalyticsProvider
+    {
+        public ParseAnalyticsProvider()
+        {
+        }
+
+        #region IAnalyticsProvider implementation
+
+        public async Task RecordTrialStarted(string trialName, string targetName)
+        {
+            var trialData = new Dictionary<string, string> {
+				{"targetName", targetName }
+			};
+            await ParseAnalytics.TrackEventAsync(string.Format("{0}-Started", trialName), trialData);
+        }
+
+        public async Task RecordTrialSucceeded(string trialName, string targetName)
+        {
+            var trialData = new Dictionary<string, string> {
+				{"targetName", targetName }
+			};
+            await ParseAnalytics.TrackEventAsync(string.Format("{0}-Succeeded", trialName), trialData);
+        }
+
+        #endregion
+    }
+}
---
+++ SwitchAB.Samples.WP8/Bootstrap/SqlitePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace SwitchAB.Samples.WP8.Bootstrap
+{
+    public class SqlitePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Sqlite.PluginLoader>
+    {
+    }
+}
